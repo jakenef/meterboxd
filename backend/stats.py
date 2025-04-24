c483e80945ee554c02aee394dd0900c4aba31700
Jacob Nef <jacob@nef-family.net>
2025-04-23 20:42:47 -0600

    implement MovieData class and begin implementing getRatingData method

--- backend/stats.py
+++ backend/stats.py
@@ -1,5 +1,25 @@
 from backend.csvReader import load_ratings_csv
+from dataclasses import dataclass
+@dataclass
+class MovieData:
+    title: str
+    year: int
+    publicRating: float
+    userRating: float
+    popularityScore: float
 
 def getStats(file):
     csv_data = load_ratings_csv(file)
+
     return csv_data
+
+def getRatingData(csv_data):
+    maxDifMovie = None
+    minDifMovie = None
+    for entry in csv_data:
+        publicMovieRating = getPublicMovieData(entry)
+        difference = publicMovieRating - entry['rating']
+        if (difference > maxDif):
+            maxDif = dif
+
+
