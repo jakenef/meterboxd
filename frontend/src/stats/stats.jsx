61e59b01d119d4cc85df3d5945a15e2f0e777a6f
jakenef <jakenef@byu.edu>
2025-08-30 15:45:57 -0600

    Fix import case for ToughCrowdSection component in stats.jsx

--- frontend/src/stats/stats.jsx
+++ frontend/src/stats/stats.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import ToughCrowdSection from "./toughcrowdsection.jsx";
+import ToughCrowdSection from "./ToughCrowdSection.jsx";
 import ObscuritySection from "./obscuritysection.jsx";
 import ModernLoadingScreen from "../components/modernloadingscreen.jsx";
 
